bd253c73cea2e24b3890474d8360a18f5768980e
Zarrar Shehzad <czarrar@gmail.com>
2010-01-01 14:23:55 -0500

    Renamed questUpdate to qUpdate to prevent name conflict with QuestUpdate

--- qUpdate.m
+++ qUpdate.m
@@ -1,5 +1,5 @@
-function q = questUpdate(q, contrast, response)
-% function q = questUpdate(q, contrast, response)
+function q = qUpdate(q, contrast, response)
+% function q = qUpdate(q, contrast, response)
     
     for ii=1:length(intensity);
         q = QuestUpdate(q, log10(contrast(ii)), response(ii));
